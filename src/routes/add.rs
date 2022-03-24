9e220a65ab35c38dc173810b2c197c983667ac5f
Lillian Armes <lily@hotgirl.zone>
2022-03-24 06:27:01 -0400

    imprv: Pass in url to /add as a path arg

--- src/routes/add.rs
+++ src/routes/add.rs
@@ -1,16 +1,11 @@
 use std::fmt::format;
 
-use actix_web::web::Data;
+use actix_web::web::{Data, Form, Path};
 use actix_web::Responder;
 
 use crate::ShortyContext;
 
-#[actix_web::post("/add")]
-pub async fn post(ctx: Data<ShortyContext>) -> impl Responder {
-    format!(
-        "{}",
-        ctx.redirect_repo
-            .create(String::from("https://google.com"))
-            .await
-    )
+#[actix_web::post("/add/{url}")]
+pub async fn post(ctx: Data<ShortyContext>, url: Path<String>) -> impl Responder {
+    format!("{}", ctx.redirect_repo.create(url.into_inner()).await)
 }
